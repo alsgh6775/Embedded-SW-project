f6de7209259dcc18e9cfab78463a335e1d1e1a45
alsgh6775 <alsgh6775@naver.com>
2018-10-16 01:01:02 -0700

    process is added

--- myapp.c
+++ myapp.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include "func1.h"
 #include "func2.h"
+
+void process()
+{
+ printf("PROCESS IS WORKING ALWAYS\n");
+}
+
 int main(void)
 {
  func1();
  func2();
+ process();
 
 }
