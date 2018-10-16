88eec376bdaf32f20cf92c19f594e0ac51b2fc0e
alsgh6775 <alsgh6775@naver.com>
2018-10-16 00:56:39 -0700

    power is add

--- myapp.c
+++ myapp.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include "func1.h"
 #include "func2.h"
+
+void power_on()
+{
+ printf("power_on\n");
+}
+
+void power_off()
+{
+ printf("power_off\n");
+}
+
 int main(void)
 {
+ power_on();
  func1();
  func2();
-
+ power_off();
 }
